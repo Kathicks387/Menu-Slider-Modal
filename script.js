40c83ab44cac86198ca4a19ed98b9aad125f45e9
Can Do <Katrinarhicks@gmail.com>
2020-12-07 08:06:45 -0500

    Created js codes

---
+++ script.js
@@ -0,0 +1,96 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+const form = document.getElementById('modal-form');
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Toggle nav
+toggle.addEventListener('click', () => document.body.classList.toggle('show-nav') );
+
+// Show modal
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+//Hide modal
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+// Hide modal on outside click
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
+
+// Modal Form Completion
+
+//Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error'
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+//Show input success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success'
+}
+
+
+
+//Check if email is valid
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())) {
+      showSuccess(input);
+    } else {
+      showError(input, 'Email is not valid');
+    }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    if(input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+//Check input length
+function checkLength(input, min, max) {
+  if(input.value.length < min){
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if(input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+}
+
+//check if passwords match
+function checkPasswordsMatch(input1, input2){
+  if(password2.value !== password.value) {
+    showError(input2, 'Passwords do not match');
+  } else{
+    showSuccess(input);
+  }
+}
+//Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+ //Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  checkRequired([name, email, password, password2]);
+  checkLength(name, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
+});
